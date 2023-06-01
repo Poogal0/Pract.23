8280da1e2cbe6e61b450f6bc33eb98f8de008309
Poogal0 <golubchikov_2002@mail.ru>
2023-06-01 11:35:53 +0400

    v5 - panorama works

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-console.log("JS maps: increment 4.");
+console.log("JS maps: increment 5.");
 var x0 = 55.790927;
 var y0 = 49.114453;
 
@@ -6,8 +6,15 @@ ymaps.ready(init);
 function init() {
 	console.log("init: called.");
 	ymaps.panorama.locate([x0,y0]).done(
-		function() {
-			console.log("panorama.locate: done.")
-		}
+		function(panoramas) {
+			//console.log("panorama.locate: done.")
+			console.log("panaramas: " + panoramas);
+			console.log("panaramas[0]: " + panoramas[0]);
+			var player = new ymaps.panorama.Player(
+												"player",
+												panoramas[0]
+			);
+		},
+		function() {}
 	);
 }
