4212c09bf7c08b4425c2554acce332937f30a2f4
Poogal0 <golubchikov_2002@mail.ru>
2023-06-01 11:34:03 +0400

    v4 - panorama loc

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
-console.log("JS maps: increment 3.");
+console.log("JS maps: increment 4.");
+var x0 = 55.790927;
+var y0 = 49.114453;
+
 ymaps.ready(init);
 function init() {
 	console.log("init: called.");
+	ymaps.panorama.locate([x0,y0]).done(
+		function() {
+			console.log("panorama.locate: done.")
+		}
+	);
 }
