8890d3dd917593153790efddb0d0ec0f5b43d5ba
Poogal0 <golubchikov_2002@mail.ru>
2023-06-01 11:39:32 +0400

    v7 - button "Back"

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
-console.log("JS maps: increment 6.");
+console.log("JS maps: increment 7.");
 var x0 = 55.790927;
 var y0 = 49.114453;
 
+var x1 = 55.797391;
+var y1 = 49.105993;
+
+var player1;
 ymaps.ready(init);
 function init() {
 	console.log("init: called.");
@@ -14,6 +18,7 @@ function init() {
 												"player",
 												panoramas[0]
 			);
+			player1 = player;
 		},
 		function() {}
 	);
@@ -21,4 +26,9 @@ function init() {
 
 function onClickNext() {
 	console.log("clicked Next");
+	player1.moveTo([x1,y1]);
+}
+
+function onClickBack() {
+	console.log("clicked Back");
 }
